35719f68368aaa87ee8fbd9dd61452241caf1f1c
Daniel Zoba/Sobe <d.zoba@zalozba.de>
2025-04-24 09:02:28 +0200

    add darkmode to tailwind

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class', // 'media' oder 'class' – wir nutzen 'class'
     content: [
       "./src/**/*.{js,ts,jsx,tsx}", // alle Dateien im src-Ordner
     ],
